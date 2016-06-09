e9ed016bc1e150231d3aed7c26f4c51fdbe2874d
Yuvraj Chouhan <dilip.chouhan7@gmail.com>
2016-06-09 17:15:42 +0530

    pant controller

--- app/controllers/pants_controller.rb
+++ app/controllers/pants_controller.rb
@@ -1,3 +1,17 @@
 class PantsController < ApplicationController
 
+def create
+	@pant = current_user.pants.build(pant_params)
+	@pant.save
+
+end
+def new
+	@pant = current_user.pants.build
+	
+end
+private
+def pant_params
+	params.require(:pant).permit(:Full_length, :Waist, :Hip, :Thigh, :Knee_length, :Knee_diameter, :Folik, :Long_belt, :short_belt, :side_stich, :bottom_stich, :Pockets, :Back_pocket, :cover, :product_id, :user_id)
+	
+end
 end
